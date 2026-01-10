122cc305757b4d2cc264532d79ed889945e30d01
Preetham-c <Preethamchannup@gmail.com>
2026-01-10 18:15:47 +0530

    Add Lenis smooth scrolling for premium scroll experience

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+/* ===== LENIS SMOOTH SCROLL ===== */
+const lenis = new Lenis({
+  duration: 1.1,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smoothWheel: true,
+  smoothTouch: false
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+/* Sync Lenis with GSAP */
+lenis.on('scroll', ScrollTrigger.update);
+gsap.ticker.add((time) => {
+  lenis.raf(time * 1000);
+});
+gsap.ticker.lagSmoothing(0);
 gsap.registerPlugin(ScrollTrigger);
 
 /* HERO */
