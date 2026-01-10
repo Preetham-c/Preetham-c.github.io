129508574b5a3dfe0fa94e055e77d47b147b7e2a
Preetham-c <Preethamchannup@gmail.com>
2026-01-10 18:20:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-/* ===== LENIS SMOOTH SCROLL ===== */
-const lenis = new Lenis({
-  duration: 1.1,
-  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-  smoothWheel: true,
-  smoothTouch: false
-});
-
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-requestAnimationFrame(raf);
-
-/* Sync Lenis with GSAP */
-lenis.on('scroll', ScrollTrigger.update);
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000);
-});
-gsap.ticker.lagSmoothing(0);
 gsap.registerPlugin(ScrollTrigger);
 
 /* HERO */
